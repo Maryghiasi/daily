975a6412d7b0144053f0a5cb0667b6e7d01afd79
Maryam Ghiasi <maryamghiasi2322@gmail.com>
2021-08-01 07:34:03 +0100

    build  afolder

---
+++ daily/src/page/HomePage.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+
+
+class Homepage extends React.Component {
+    render() {
+        // var
+        const {
+            //
+        } = this.props
+        return(
+
+        <div className='root'>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Paper className='paper'>xs=12</Paper>
+          </Grid>
+          <Grid item xs={9} >
+            <Paper className='paper'>andaz9</Paper>
+          </Grid>
+          <Grid item xs={3} >
+            <Paper className='paper'>andaz3</Paper>
+          </Grid>
+        </Grid>
+      </div>
+
+        )
+    };
+};
+
+Homepage.propTypes = {
+
+}
+Homepage.defaultProps = {}
+
+export default Homepage;
