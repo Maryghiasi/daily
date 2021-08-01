d2b8a499f735c513ed477ec17d7294c4e89c0b72
Maryam Ghiasi <maryamghiasi2322@gmail.com>
2021-08-01 08:41:49 +0100

    edit

--- daily/src/page/HomePage.js
+++ daily/src/page/HomePage.js
@@ -3,6 +3,8 @@ import React from "react";
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
+import DailyExpenses from "../components/DailyExpenses";
+
 
 
 class Homepage extends React.Component {
@@ -11,21 +13,21 @@ class Homepage extends React.Component {
         const {
             //
         } = this.props
-        return(
+        return (
 
-        <div className='root'>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Paper className='paper'>xs=12</Paper>
-          </Grid>
-          <Grid item xs={9} >
-            <Paper className='paper'>andaz9</Paper>
-          </Grid>
-          <Grid item xs={3} >
-            <Paper className='paper'>andaz3</Paper>
-          </Grid>
-        </Grid>
-      </div>
+            <div className='root'>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Paper className='paper'>xs=12</Paper>
+                    </Grid>
+                    <Grid item xs={9} >
+                        <DailyExpenses/>
+                    </Grid>
+                    <Grid item xs={3} >
+                        <Paper className='paper'>andaz3</Paper>
+                    </Grid>
+                </Grid>
+            </div>
 
         )
     };
